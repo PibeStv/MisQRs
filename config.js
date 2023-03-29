82efe78ff668958d5ddeb6b83f23321a66c47383
PibeStv <128749856+PibeStv@users.noreply.github.com>
2023-03-29 01:01:00 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbxxmqkf7X6zKSpQ5sKVKEP5Tr2goxPO0F21IAxMeIPyjK4ORmbJEaXST1NKhLPoXAGifw/exec";
